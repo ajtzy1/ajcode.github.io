bd9a0af48b13e201127201276205c1c93847f525
ajtzy1 <149063902+ajtzy1@users.noreply.github.com>
2024-05-13 17:43:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () => {
+question.innerHTML = "If No, Don't Worry Positive Potato Is Here😊";
+gif.src =
+"https://github.com/ajtzy1/GeeksForGeeks/blob/main/README.md";
+});
+noBtn.addEventListener("mouseover", () => {
+const noBtnRect = noBtn.getBoundingClientRect();
+const maxX = window.innerWidth - noBtnRect.width;
+const maxy = window.innerHeight - noBtnRect.height;
+const randomX = Math.floor(Math.random() * maxX);
+const randomY = Math.floor(Math.random() * maxY);
+noBtn.style.left = randomX + "px";
+noBtn.style.top = randomy + "px";
+});
