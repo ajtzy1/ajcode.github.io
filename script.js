0d880394b54f76a7a0cc37862e8213f8ed713ec1
ajtzy1 <149063902+ajtzy1@users.noreply.github.com>
2024-05-25 14:19:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
 question.innerHTML = "If No, Don't Worry Positive Potato Is Here😊";
-gif.src ="<iframe src="https://giphy.com/embed/12fWcohsEln5V6" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/manga-kimi-ni-todoke-12fWcohsEln5V6">via GIPHY</a></p>";
+gif.src ="<iframe src="<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/12fWcohsEln5V6" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/manga-kimi-ni-todoke-12fWcohsEln5V6">via GIPHY</a></p>";
 });
 noBtn.addEventListener("mouseover", () => {
 const noBtnRect = noBtn.getBoundingClientRect();
